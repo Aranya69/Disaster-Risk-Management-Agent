4d6903c01332fd64a643f5b7e63ec5701f9824b5
Aranya69 <149767928+Aranya69@users.noreply.github.com>
2026-05-11 13:20:50 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyATb_BFstWDrhDyRP6OCFEW9Pnn5JLWvjs")
-
-for model in genai.list_models():
-    if 'generateContent' in model.supported_generation_methods:
-        print(model.name)
